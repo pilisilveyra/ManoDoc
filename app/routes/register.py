456bc43c85fda5ad6ec2c5ee82ff41e236b669d0
Pilar Silveyra <psilveyra@mail.austral.edu.ar>
2025-05-22 12:16:07 -0300

    Cambio de rutas

--- app/routes/register.py
+++ app/routes/register.py
@@ -1,10 +1,14 @@
-from flask import Blueprint, request, redirect, url_for
+from flask import Blueprint, request, redirect, url_for, render_template
 from app.extensions import db
 from app.models.Paciente import Paciente
 from app.models.Doctor import Doctor
 from werkzeug.security import generate_password_hash
 
 register_bp = Blueprint('register_bp', __name__)
+
+@register_bp.route('/register')
+def register():
+    return render_template('register.html')
 
 @register_bp.route('/registrar_paciente', methods=['POST'])
 def registrar_paciente():
