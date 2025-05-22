58e76f83b0b7f67396ebc625f967d0f478887b52
Pilar Silveyra <psilveyra@mail.austral.edu.ar>
2025-05-22 12:22:42 -0300

    registrarse como paciente y como doctor

--- app/models/Doctor.py
+++ app/models/Doctor.py
@@ -7,7 +7,7 @@ class Doctor(db.Model):
     nombre = db.Column(db.String(100), nullable=False)
     apellido = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    contrasena = db.Column(db.String(100), nullable=False)
+    contrasena = db.Column(db.String(256), nullable=False)
     dni = db.Column(db.String(8), unique=True, nullable=False)
     especialidad = db.Column(db.String(120), nullable=False)
 
