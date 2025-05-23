53875bdd3290006a51778e23c7eff6278cc03f8e
Pilar Silveyra <psilveyra@mail.austral.edu.ar>
2025-05-23 12:09:27 -0300

    pagina paciente

--- app/models/Turno.py
+++ app/models/Turno.py
@@ -8,12 +8,10 @@ class Turno(db.Model):
     fecha = db.Column(db.Date, nullable=False)
     hora = db.Column(db.Time, nullable=False)
 
-    id_paciente = db.Column(db.Integer, db.ForeignKey('pacientes.id_paciente'), nullable=False)
-    id_doctor = db.Column(db.Integer, db.ForeignKey('doctores.id_doctor'), nullable=False)
+    id_paciente = db.Column(db.Integer, db.ForeignKey('paciente.id_paciente'), nullable=False)
+    id_doctor = db.Column(db.Integer, db.ForeignKey('doctor.id_doctor'), nullable=False)
 
     # Relaciones (opcionales pero útiles)
     paciente = db.relationship('Paciente', backref=db.backref('turnos', lazy=True))
     doctor = db.relationship('Doctor', backref=db.backref('turnos', lazy=True))
 
-    def __repr__(self):
-        return f"<Turno {self.fecha} {self.hora} - Paciente {self.id_paciente} - Doctor {self.id_doctor}>"
