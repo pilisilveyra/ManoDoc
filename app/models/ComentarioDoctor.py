e3cf012bb08308a0dd9ddbc77a596900adafcbf1
Pilar Silveyra <psilveyra@mail.austral.edu.ar>
2025-06-06 18:17:41 -0300

    .

--- app/models/ComentarioDoctor.py
+++ app/models/ComentarioDoctor.py
@@ -5,6 +5,6 @@ from app.extensions import db
 class ComentarioDoctor(db.Model):
     __tablename__ = 'ComentarioDoctor'
     id_comentario = db.Column(db.Integer, primary_key=True)
-    id_operacion = db.Column(db.Integer, db.ForeignKey('operacion.id_operacion'), nullable=False)
+    id_operacion = db.Column(db.Integer, db.ForeignKey('operaciones.id_operacion'), nullable=False)
     contenido = db.Column(db.String(500), nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
