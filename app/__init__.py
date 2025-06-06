7332d07dc264e540367f6fa31c54e444b4d72758
Pilar Silveyra <psilveyra@mail.austral.edu.ar>
2025-06-06 10:04:24 -0300

    agrego pagina ppal doctor

--- app/__init__.py
+++ app/__init__.py
@@ -24,6 +24,7 @@ def create_app():
         from app.models.Paciente import Paciente
         from app.models.Turno import Turno
         from app.models.Temperatura import Temperatura
+        from app.models.Operacion import Operacion
         db.create_all()
 
     app.register_blueprint(register_bp)
